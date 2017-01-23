a27c536d34946ca00b0e8ff7f2f5e255e7d47bbf
Petko Chakov <petko.chakov@gmail.com>
2017-01-23 22:30:14 +0200

    BeforeClass

---
+++ PracticeCharactersAndStrings/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ExchangeVariableValues
+{
+	class MainClass
+	{
+		public static void Main(string[] args)
+		{
+			int a = int.Parse(Console.ReadLine());
+			int b = int.Parse(Console.ReadLine());
+			int temp = a;
+			a = b;
+			b = temp;
+			Console.WriteLine(a);
+			Console.WriteLine(b);
+
+		}
+	}
+}
---
+++ PracticeFloatingPointNumbers/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace PracticeFloatingPointNumbers
+{
+	class MainClass
+	{
+		public static void Main(string[] args)
+		{
+
+			decimal a = 3.141592653589793238m;
+			decimal b = 1.60217657m;
+			decimal c = 7.8184261974584555216535342341m;
+			Console.WriteLine(a);
+			Console.WriteLine(b);
+			Console.WriteLine(c);	
+
+		}
+	}
+}
