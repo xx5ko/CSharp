c3de135e3fadd7dea4b4f503937e0ee8e044ab24
Petko Chakov <petko.chakov@gmail.com>
2017-01-30 02:07:03 +0200

    Commit

--- ASCIIString/Program.cs
+++ ASCIIString/Program.cs
@@ -6,6 +6,16 @@ namespace ASCIIString
 	{
 		public static void Main(string[] args)
 		{
-
-
+			var number = int.Parse(Console.ReadLine());
+			string result = "";
+			for (int i = 0; i < number; i++)
+			{
+				char ConvertToChar = (char)int.Parse(Console.ReadLine());
+				result = result + ConvertToChar;
+			}
+			Console.WriteLine(result);
 		}
+	}
+}
+
+	
