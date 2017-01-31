a9a4c4278ee7734840c573a3b8fad5fbeb9706af
Petko Chakov <petko.chakov@gmail.com>
2017-01-31 21:01:32 +0200

    MethodsEx

--- NthDigit/Program.cs
+++ NthDigit/Program.cs
@@ -1,31 +1,13 @@
-﻿﻿using System;
+﻿using System;
 using System.Collections.Generic;
-using System.Security.Cryptography;
+
 namespace NthDigit
 {
     internal class Program
     {
         public static void Main(string[] args)
         {
-            var number = long.Parse(Console.ReadLine());
-            var index = int.Parse(Console.ReadLine());
 
-            var digitAtIndex = FindDigit(number, index);
-            Console.WriteLine(digitAtIndex);
-        }
-        static int FindDigit(long number, int index)
-        {
-            var currentIndex = 1;
-            while (number > 0)
-            {
-                if (currentIndex == index)
-                {
-                    return (int) (number % 10);
-                }
-                currentIndex++
-                number /= 10;
-            }
-            return (int) (number % 10);
         }
     }
 }
