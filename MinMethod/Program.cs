6cff8aefdafce1d9da036dc1a3a5355c2296237a
Petko Chakov <petko.chakov@gmail.com>
2017-01-31 12:39:15 +0200

    MethodsEx

--- MinMethod/Program.cs
+++ MinMethod/Program.cs
@@ -6,7 +6,29 @@ namespace MinMethod
 	{
 		public static void Main(string[] args)
 		{
-			Console.WriteLine("Hello World!");
+			int a = int.Parse(Console.ReadLine());
+			int b = int.Parse(Console.ReadLine());
+			int c = int.Parse(Console.ReadLine());
+
+			int firstMinimum = GetMin(a, b, c);
+			Console.WriteLine();
+			Console.WriteLine(firstMinimum);
+		}
+
+
+		static int GetMin(int a, int b, int c)
+		{
+			int firstMinimum = Math.Min(a, b);
+
+			if (firstMinimum > c)
+			{
+				return c;
+			}
+			else
+			{
+				return firstMinimum;
+			}
 		}
 	}
 }
+
---
+++ StringRepater/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace StringRepater
+{
+	class MainClass
+	{
+		public static void Main(string[] args)
+		{
+			string str = Console.ReadLine();
+			int number = int.Parse(Console.ReadLine());
+
+			string repeated = RepearString(str,number);
+
+			Console.WriteLine(repeated);
+		}
+
+		static string RepearString(string str, int number)
+		{
+			string stringToRepeat = str;
+
+			for (int i = 0; i < number; i++)
+
+			{
+				Console.Write(stringToRepeat, number);
+			}
+			return stringToRepeat;
+
+	}
+	}
+}
+
