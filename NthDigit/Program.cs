98060ae9aa7de84bc1583822e50193234336c016
Petko Chakov <petko.chakov@gmail.com>
2017-01-31 19:17:26 +0200

    Clone Test

--- NthDigit/Program.cs
+++ NthDigit/Program.cs
@@ -1,14 +1,37 @@
 ﻿using System;
+using System.Globalization;
 
 namespace NthDigit
 {
-	class MainClass
-	{
-		public static void Main(string[] args)
-		{
-			//
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
 
+            var number = long.Parse(Console.ReadLine());
+            var index = int.Parse(Console.ReadLine());
+            var digitAtIndex = FindNthDigit(number, index);
 
-		}
-	}
+            Console.WriteLine(digitAtIndex);
+
+        }
+
+        static int FindNthDigit(long number, int index)
+        {
+            var currentIndex = 1;
+
+            while (number > 0)
+            {
+                if (currentIndex == index)
+                {
+
+                    return (int) (number % 10);
+                }
+
+                currentIndex++
+                ((int)(number /= 10));
+            }
+            return (int) (number % 10);
+        }
+    }
 }
