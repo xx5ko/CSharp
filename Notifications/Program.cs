745babae55ba84c8294774739b1f090af49a5411
Petko Chakov <petko.chakov@gmail.com>
2017-02-01 16:29:05 +0200

    MethodsEx

---
+++ Notifications/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Notifications
+{
+    internal class Program
+    {
+        for (int i = 0; i < number; i++)
+    {
+    string result = Console.ReadLine();
+        if (result=="success")
+    {
+        string operation = Console.ReadLine();
+        string message = Console.ReadLine();
+
+        Console.WriteLine(ShowSuccess(operation,message));
+    }
+    else if (result=="error")
+    {
+    string operation = Console.ReadLine();
+    int code = int.Parse(Console.ReadLine());
+
+    Console.WriteLine(ShowError(operation, code));
+    }
+    else
+    {
+    continue;
+}
+}
+}
+public static string ShowSuccess(string operation,string message)
+{
+
+string output = ($"Successfully executed {operation}.\n==============================\nMessage: {message}.");
+return output;
+}
+public static string ShowError(string operation, int code)
+{
+if (code<0)
+{
+string output = ($"Error: Failed to execute {operation}.\n==============================\nError Code: {code}.\nReason: Internal System Failure.");
+return output;
+}
+else
+{
+string output = ($"Error: Failed to execute {operation}.\n==============================\nError Code: {code}.\nReason: Invalid Client Data.");
+return output;
+}
+}
+
+}
