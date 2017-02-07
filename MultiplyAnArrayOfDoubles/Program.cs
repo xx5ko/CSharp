0231a23a06cc6dedd0182d2bad66040fbae20035
Petko Chakov <petko.chakov@gmail.com>
2017-02-07 12:29:31 +0200

    Add files via upload

---
+++ MultiplyAnArrayOfDoubles/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MultiplyAnArrayOfDoubles
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+
+            var stringArr = Console.ReadLine().Split();
+            var arr = new double[stringArr.Length];
+
+            for (int i = 0; i < arr.Length; i++)
+                arr[i] = double.Parse(stringArr[i]);
+
+            var p = double.Parse(Console.ReadLine());
+            for (int i = 0; i < arr.Length; i++)
+                arr[i] *= p;
+            for (int i = 0; i < arr.Length; i++)
+
+                Console.Write(arr[i] + " ");
+
+        }
+    }
+}
