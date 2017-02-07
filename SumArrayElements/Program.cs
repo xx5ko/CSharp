a3fe3f6ddcd0e43936d85206d12104c6e7010bad
Petko Chakov <petko.chakov@gmail.com>
2017-02-07 12:17:56 +0200

    Add files via upload

---
+++ SumArrayElements/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace SumArrayElements
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+
+            var numbers = int.Parse(Console.ReadLine());
+            var array = new int[numbers];
+
+            for (int i = 0; i < array.Length; i++)
+                array[i] = int.Parse(Console.ReadLine());
+            var sum = 0;
+            for (int i = 0; i < array.Length; i++)
+                sum += array[i];
+
+            Console.WriteLine(sum);
+        }
+    }
+}
