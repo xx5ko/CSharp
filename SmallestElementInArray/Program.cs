486b7d541058aa8dde996cc408c1c3b492ec1c49
Petko Chakov <petko.chakov@gmail.com>
2017-02-07 12:43:05 +0200

    Add files via upload

---
+++ SmallestElementInArray/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace SmallestElementInArray
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+
+            var stringArray = Console.ReadLine().Split();
+            var array = new int[stringArray.Length];
+
+            for (int i = 0; i < array.Length; i++)
+                array[i] = int.Parse(stringArray[i]);
+
+            var smallest = int.MaxValue;
+            for (int i = 0; i < array.Length; i++)
+                if (array[i] < smallest)
+                    smallest = array[i];
+            Console.WriteLine(smallest);
+
+        }
+    }
+}
