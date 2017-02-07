a91ce46dedef9d499be326109219586748a1d9a5
Petko Chakov <petko.chakov@gmail.com>
2017-02-07 12:56:39 +0200

    Add files via upload

---
+++ RotateArrayOfStrings/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+
+namespace RotateArrayOfStrings
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            var array = Console.ReadLine().Split().ToArray();
+            var rotatedArray = new string[array.Length];
+            for (int i = 0; i < array.Length - 1; i++)
+                rotatedArray[i + 1] = array[i];
+            var lastElement = array[rotatedArray.Length - 1];
+            rotatedArray[0] = lastElement;
+            Console.WriteLine(string.Join(" ", rotatedArray));
+        }
+    }
+}
