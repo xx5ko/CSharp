c528483895370964e6376b15c9d8605231a25ab8
Petko Chakov <petko.chakov@gmail.com>
2017-02-15 21:33:17 +0200

    ListsEx

---
+++ Phonebook/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Phonebook
+{
+	class MainClass
+	{
+		public static void Main()
+		{
+			string[] phoneNumbers = Console.ReadLine().Split(' ');
+			string[] names = Console.ReadLine().Split(' ');
+
+			var result = string.Empty;
+
+			for (int i = 0; ; i++)
+			{
+				var name = Console.ReadLine();
+
+				if (name == "done")
+				{
+					break;
+				}
+
+				for (int i2 = 0; i2 < names.Length; i2++)
+				{
+					if (name == names[i2])
+					{
+						result += $"{names[i2]} -> {phoneNumbers[i2]}" + Environment.NewLine;
+					}
+				}
+			}
+
+			Console.WriteLine(result.Remove(result.Length - 1));
+		}
+	}
+}
