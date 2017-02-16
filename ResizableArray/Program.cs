3275bcf7c9e1f03be5b1a7d30de27dc23d197e64
Petko Chakov <petko.chakov@gmail.com>
2017-02-16 21:14:55 +0200

    ArraysEx

---
+++ ResizableArray/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ResizableArray
+{
+	class ResizableArray
+	{
+		static void Main(string[] args)
+		{
+			string[] input = Console.ReadLine().Split(' ').ToArray();
+
+			List<int> result = new List<int>();
+
+			while (!input[0].Equals("end"))
+			{
+
+				if (input[0].Equals("push"))
+				{
+					result.Add(int.Parse(input[1]));
+				}
+				else if (input[0].Equals("pop"))
+				{
+					result.RemoveAt(result.Count - 1);
+				}
+				else if (input[0].Equals("removeAt"))
+				{
+					result.RemoveAt(int.Parse(input[1]));
+				}
+				else if (input[0].Equals("clear"))
+				{
+					result.Clear();
+				}
+
+				input = Console.ReadLine().Split(' ').ToArray();
+			}
+
+			Console.WriteLine(string.Join(" ", result));
+		}
+	}
+}
