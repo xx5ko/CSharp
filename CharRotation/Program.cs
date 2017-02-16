8cab9a5f48ab9f70439d1aabe7d0774986f096f5
Petko Chakov <petko.chakov@gmail.com>
2017-02-16 21:38:08 +0200

    ArraysEx

--- CharRotation/Program.cs
+++ CharRotation/Program.cs
@@ -1,12 +1,31 @@
 ﻿using System;
-
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 namespace CharRotation
 {
 	class MainClass
 	{
 		public static void Main(string[] args)
 		{
-			Console.WriteLine("Hello World!");
+			string text = Console.ReadLine();
+
+			int[] numbers = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+
+			string result = "";
+			for (int i = 0; i < text.Length; i++)
+			{
+				if (numbers[i] % 2 == 0)
+				{
+					result += (char)(text[i] - numbers[i]);
+				}
+				else
+				{
+					result += (char)(text[i] + numbers[i]);
+				}
+			}
+			Console.WriteLine(result);
 		}
 	}
 }
