de06d1cedd862da0fe3b6d14693848a628198115
Petko Chakov <petko.chakov@gmail.com>
2017-02-16 21:33:50 +0200

    ArraysEx

---
+++ ArrayElementsEqualToTheirIndex/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArrayElementsEqualToTheirIndex
+{
+	class MainClass
+	{
+		 static void Main(string[] args)
+		{
+			int[] array = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+
+			for (int i = 0; i < array.Length; i++)
+			{
+				if (array[i] == i)
+				{
+					Console.Write(array[i] + " ");
+				}
+			}
+			Console.WriteLine();
+		}
+	}
+}
---
+++ Last3ConsecutiveEqualStrings/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Last3ConsecutiveEqualStrings
+{
+	class MainClass
+	{
+		public static void Main(string[] args)
+		{
+			string[] array = Console.ReadLine().Split(' ');
+
+			int count = 1;
+
+			for (int i = array.Length - 1; i > 0; i--)
+			{
+				if (array[i] == array[i - 1])
+				{
+					count++;
+
+					if (count == 3)
+					{
+						Console.WriteLine("{0} {0} {0}", array[i]);
+						break;
+					}
+				}
+				else
+				{
+					count = 1;
+				}
+			}
+		}
+	}
+}
