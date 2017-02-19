fd2cf7158547a146a077973cdd6d43bf1d1ee131
Petko Chakov <petko.chakov@gmail.com>
2017-02-19 15:11:14 +0200

    ListsEx

---
+++ FlipListSides/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlipListSides
+{
+	class FlipListSides
+	{
+		static void Main(string[] args)
+		{
+			List<int> input = Console.ReadLine().Split().Select(int.Parse).ToList();
+
+
+			for (int i = 1; i < input.Count / 2; i++)
+			{
+				int temp = input[i];
+				input[i] = input[input.Count - i - 1];
+				input[input.Count - i - 1] = temp;
+			}
+
+			Console.WriteLine(string.Join(" ", input));
+		}
+	}
+}
