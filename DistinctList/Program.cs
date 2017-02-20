156dd942d20a60c368984becb3809ddcb045342d
Petko Chakov <petko.chakov@gmail.com>
2017-02-20 20:07:30 +0200

    ListsEx

---
+++ DistinctList/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq;
+
+namespace DistinctList
+{
+	class MainClass
+	{
+		public static void Main(string[] args)
+		{
+			var numbers = Console.ReadLine().Split(' ').Select(int.Parse).ToList();
+
+			for (int i = 0; i < numbers.Count; i++)
+			{
+				for (int j = i + 1; j < numbers.Count; j++)
+				{
+					if (numbers[i] == numbers[j])
+					{
+						numbers.RemoveAt(j);
+						i = 0;
+					}
+				}
+			}
+
+			Console.WriteLine(string.Join(" ", numbers));
+		}
+	}
+}
